d9b1de869d1c68542fcd4ca46a30e4b72b893e15
zzk <510282904@qq.com>
2019-12-03 17:13:58 +0800

    新建User.java，提交到本地仓库

---
+++ git_day01_project/src/main/java/com/itzzk/bean/User.java
@@ -0,0 +1,11 @@
+package com.itzzk.bean;
+
+/**
+ * @author: zzk
+ * @date: 2019/12/3
+ * @darctiption: com.itzzk.bean
+ */
+public class User {
+    private Integer id;
+    private String name;
+}
